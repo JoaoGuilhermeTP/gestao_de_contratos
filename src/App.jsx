43e566cf71e9c61e285ab8c5bacba7e589aefa31
João Guilherme <adm.joaoguilherme@gmail.com>
2026-09-16 23:23:14 -0300

    Added routes navigation with React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 // import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ContratosPage from "./pages/contratosPage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/NavBar";
 
 function App() {
-
   return (
-    <>
-      <ContratosPage />
-    </>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contratos" element={<ContratosPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
