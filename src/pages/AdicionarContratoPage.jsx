b3b67de0c9822ec4afdad934e5a5ba033dcc9eb5
João Guilherme <adm.joaoguilherme@gmail.com>
2026-09-22 22:10:40 -0300

    Incluidos rótulos para os campos do formulário de criação de novo contrato

--- src/pages/AdicionarContratoPage.jsx
+++ src/pages/AdicionarContratoPage.jsx
@@ -31,42 +31,128 @@ export default function AddContratoForm({ contratos, setContratos, fornecedores,
       <h2>Adicionar novo contrato</h2>
       <button>Importar arquivo</button>
       <form onSubmit={handleSubmit}>
-        <input type="number" placeholder="Número" value={novoContrato.numero} onChange={(e) => setNovoContrato({ ...novoContrato, numero: e.target.value })} />
-        <input type="number" placeholder="Ano" value={novoContrato.ano} onChange={(e) => setNovoContrato({ ...novoContrato, ano: e.target.value })} />
-        <input type="text" placeholder="Objeto" value={novoContrato.objeto} onChange={(e) => setNovoContrato({ ...novoContrato, objeto: e.target.value })} />
-        <select
-          value={novoContrato.fornecedor_id}
-          onChange={(e) =>
-            setNovoContrato({
-              ...novoContrato,
-              fornecedor_id: Number(e.target.value),
-            })
-          }
-        >
-          <option value="" disabled>
-            Selecione um fornecedor
-          </option>
-          {fornecedores.map((fornecedor) => (
-            <option key={fornecedor.id} value={fornecedor.id}>
-              {fornecedor.nome} - {fornecedor.cnpj}
-            </option>
-          ))}
-        </select>
+        <label>
+          Número do contrato
+          <input
+            type="number"
+            value={novoContrato.numero}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                numero: e.target.value,
+              })
+            }
+          />
+        </label>
 
-        <input type="text" placeholder="procedimento" value={novoContrato.procedimento} onChange={(e) => setNovoContrato({ ...novoContrato, procedimento: e.target.value })} />
-        <input type="text" placeholder="tipo" value={novoContrato.tipo} onChange={(e) => setNovoContrato({ ...novoContrato, tipo: e.target.value })} />
-        <input
-          type="date"
-          value={novoContrato.dataDaAssinatura}
-          onChange={(e) =>
-            setNovoContrato({
-              ...novoContrato,
-              dataDaAssinatura: e.target.value,
-            })
-          }
-        />
-        <input type="date" value={novoContrato.dataDeTermino} onChange={(e) => setNovoContrato({ ...novoContrato, dataDeTermino: e.target.value })} />
-        <button>Adicionar</button>
+        <label>
+          Ano
+          <input
+            type="number"
+            value={novoContrato.ano}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                ano: e.target.value,
+              })
+            }
+          />
+        </label>
+
+        <label>
+          Objeto
+          <input
+            type="text"
+            value={novoContrato.objeto}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                objeto: e.target.value,
+              })
+            }
+          />
+        </label>
+
+        <label>
+          Fornecedor
+          <select
+            value={novoContrato.fornecedor_id}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                fornecedor_id: Number(e.target.value),
+              })
+            }
+          >
+            <option value="" disabled>
+              Selecione um fornecedor
+            </option>
+
+            {fornecedores.map((fornecedor) => (
+              <option key={fornecedor.id} value={fornecedor.id}>
+                {fornecedor.nome} - {fornecedor.cnpj}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label>
+          Procedimento
+          <input
+            type="text"
+            value={novoContrato.procedimento}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                procedimento: e.target.value,
+              })
+            }
+          />
+        </label>
+
+        <label>
+          Tipo de contrato
+          <input
+            type="text"
+            value={novoContrato.tipo}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                tipo: e.target.value,
+              })
+            }
+          />
+        </label>
+
+        <label>
+          Data da assinatura
+          <input
+            type="date"
+            value={novoContrato.dataDaAssinatura}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                dataDaAssinatura: e.target.value,
+              })
+            }
+          />
+        </label>
+
+        <label>
+          Data de término
+          <input
+            type="date"
+            value={novoContrato.dataDeTermino}
+            onChange={(e) =>
+              setNovoContrato({
+                ...novoContrato,
+                dataDeTermino: e.target.value,
+              })
+            }
+          />
+        </label>
+
+        <button type="submit">Adicionar</button>
       </form>
     </>
   );
